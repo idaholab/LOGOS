a5cbe9d76ea9329d7f500f7595d35a077e284d01
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2021-01-28 08:24:10 -0700

    Adding single script to make all documents.

---
+++ doc/make_docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_NAME=`readlink $0`
+if test -x "$SCRIPT_NAME";
+then
+    SCRIPT_DIRNAME=`dirname $SCRIPT_NAME`
+else
+    SCRIPT_DIRNAME=`dirname $0`
+fi
+SCRIPT_DIR=`(cd $SCRIPT_DIRNAME; pwd)`
+
+set -o errexit
+
+cd $SCRIPT_DIR/user_manual/
+make
+
+#sqa manual
+cd $SCRIPT_DIR/sqa/
+./make_docs.sh
+
