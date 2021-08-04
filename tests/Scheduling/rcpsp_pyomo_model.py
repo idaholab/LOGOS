0562b833b61b393293a993317dc7f48f075eaf7f
Wang, Congjian <congjian.wang@inl.gov>
2021-08-03 22:50:06 -0600

    update model

--- tests/Scheduling/rcpsp_pyomo_model.py
+++ tests/Scheduling/rcpsp_pyomo_model.py
@@ -13,48 +13,39 @@ model.Jm = Param(within=NonNegativeIntegers) # Total jobs + 2 artificial jobs
 model.Rm = Set() # set of resources
 model.T = RangeSet(1, model.Tm) # time periods
 model.J = RangeSet(1, model.Jm) # jobs
-model.Kr = Param(model.Rm) # number of units of renewable resource r available in perior t
+model.Kr = Param(model.Rm) # number of units of renewable resource r available in period t
 model.D = Param(model.J) # duration of job j
 model.Kjr = Param(model.J, model.Rm) # number of units of renewable resources r consumed by job j while in process
-model.JP = Set(dimen=2)
-model.Pred = Param(model.JP)
-
-def NodesIn_init(model, jp):
-  retval = []
-  for (j, p) in model.JP:
-    if j == jp:
-      retval.append(p)
-  return retval
-model.Predecessors = Set(model.J, initialize=NodesIn_init)
+model.JP = Set(dimen=2) # set of precedcessors
+model.Pred = Param(model.JP) # immediate predecessors
 
 ################# variables
 model.x = Var(model.J,model.T, domain=NonNegativeIntegers, bounds=(0,1)) #
-
 
 ################## Objective
 def obj_rule(model):
   return sum((model.T[t]-1)*model.x[model.J.last(),t] for t in model.T)
 model.minMakespan = Objective(rule=obj_rule, sense=minimize)
 
-################## Constraint
+################## Constraint on X
 
 def constraintX(model, j):
-  return sum(model.x[j, t]  for t in model.T) == 1
+  return sum(model.x[j, t]  for t in model.T) - 1 == 0
 model.constraintX = Constraint(model.J, rule=constraintX)
 
-def predecessorsConstraint(model, j, t, jp, p):
-  if j == jp:
-    return sum(model.x[j, tprime] for tprime in model.T if tprime <= t) <= sum(model.x[model.Pred[(jp,p)], tprime] for tprime in model.T if tprime <= t - model.D[j])
-  else:
-    return Constraint.Skip
-model.predecessorsConstraint = Constraint(model.J, model.T, model.JP, rule=predecessorsConstraint)
+################### Constraint on predecessors
+def predecessorsConstraint(model, jp, p, t):
+  return sum(model.x[model.Pred[(jp,p)], tprime] for tprime in model.T if tprime <= t) <= sum(model.x[jp, tprime] for tprime in model.T if tprime <= t - model.D[jp])
+model.predecessorsConstraint = Constraint(model.JP, model.T, rule=predecessorsConstraint)
 
-def resourcesConstraint(model, t, r):
-  return sum(sum(model.Kjr[j, r] * model.x[j, tprime] for tprime in model.T if tprime >= t and tprime <= t + model.D[j]-1) for j in model.J) <= model.Kr[r]
-model.resourcesConstraint = Constraint(model.T, model.Rm, rule=resourcesConstraint)
+##################### Constraint on resources
+def resourcesConstraint(model, r, t):
+  return (0, sum(sum(model.Kjr[j, r] * model.x[j, tprime] for tprime in model.T if tprime >= t and tprime <= t + model.D[j]-1) for j in model.J), model.Kr[r])
+model.resourcesConstraint = Constraint(model.Rm, model.T, rule=resourcesConstraint)
 
 # Create a model instance and optimize
-instance = model.create_instance('inputData.dat')
+instance = model.create_instance('inputData_J60.dat')
+instance.pprint()
 results = opt.solve(instance, tee=True)
 
 with open('Results.txt', 'w') as f:
