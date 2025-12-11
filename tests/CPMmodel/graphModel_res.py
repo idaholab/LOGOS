7cbd265da202da70efd4a177c52b96c06a778c16
Diego Mandelli <diego.mandelli@inl.gov>
2025-12-11 16:47:15 -0700

    resored files

--- tests/CPMmodel/graphModel_res.py
+++ tests/CPMmodel/graphModel_res.py
@@ -1,30 +1,34 @@
 from LOGOS.src.CPM.PertMain2 import Pert
 from LOGOS.src.CPM.PertMain2 import Activity
-import datetime
+import datetime as dt
 import pandas as pd
 import numpy as np
 
 class project():
-  start = Activity("start", 10, res={"res1":1})
-  b     = Activity("b",     20, res={"res1":1})
-  c     = Activity("c",      5, res={"res1":1})
-  d     = Activity("d",     10, res={"res1":1})
-  f     = Activity("f",     15, res={"res1":1})
-  g     = Activity("g",      5, res={"res1":1})
-  h     = Activity("h",     15, res={"res1":1})
-  end   = Activity("end",   20, res={"res1":1})
+  start = Activity("start", duration=2, res={'res1':1})
+  a     = Activity("a",     duration=2, res={'res1':1})
+  b     = Activity("b",     duration=3, res={'res1':1})
+  c     = Activity("c",     duration=3, res={'res1':1})
+  d     = Activity("d",     duration=4, res={'res1':1})
+  e     = Activity("e",     duration=3, res={'res1':1})
+  f     = Activity("f",     duration=2, res={'res1':1})
+  g     = Activity("g",     duration=3, res={'res1':1})
+  h     = Activity("h",     duration=4, res={'res1':1})
+  end   = Activity("end",   duration=2, res={'res1':1})
 
-  graph = {start: [f,b,h],
-           b    : [c],
-           c    : [g,d],
-           d    : [end],
-           f    : [g],
-           g    : [end],
-           h    : [end],
-           end  : []}
+  graph = {start: [a, d, f],
+          a: [b],
+          b: [c],
+          c: [g, h],
+          d: [e],
+          e: [c],
+          f: [c],
+          g: [end],
+          h: [end],
+          end:[]}
   
 class resource_schedule():
-  outageStartTime =  datetime(2025, 4, 25, 8)
+  outageStartTime = dt.datetime(2025, 10, 20, 8)
 
   N = 30
   hourly_index = pd.date_range(start=outageStartTime, periods=N, freq='h')
